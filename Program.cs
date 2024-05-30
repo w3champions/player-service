db7bdc4bb86c959680b5817bfc9d0f303d1ac676
Gustav Andersson <gustav87and@gmail.com>
2024-05-30 15:34:18 +0200

    add mongodb and signalr initial config

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using MongoDB.Driver;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// Add MongoDB
+string mongoConnectionString = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STRING") ?? "mongodb://localhost:27025";
+MongoClient mongoClient = new MongoClient(mongoConnectionString);
+builder.Services.AddSingleton(mongoClient);
+
+// Add SignalR for using websockets
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+// Configure the HTTP request pipeline
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
