0dc08472429d250beae8ffd2004bcb3f4b5c805b
Gustav Andersson <gustav87and@gmail.com>
2024-05-30 14:51:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-new-sdk-templates#webapi
+
+```shell
+dotnet new webapi --no-openapi -controllers --name player-service-net-test4
+```
+
---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace player_service_net_test4;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
