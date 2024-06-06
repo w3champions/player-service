5a12cab79df85cfd6d9fbec7b4d76bdc6fceb1c9
Gustav Andersson <gustav87and@gmail.com>
2024-06-06 13:31:44 +0200

    set MONGO_CONNECTION_STRING to empty string if no environment variable is found (#7)

--- W3ChampionsPlayerService.Tests/IntegrationTestBase.cs
+++ W3ChampionsPlayerService.Tests/IntegrationTestBase.cs
@@ -6,7 +6,6 @@ namespace W3ChampionsPlayerService.Tests;
 [TestFixture]
 public class IntegrationTestBase
 {
-    //protected readonly MongoClient MongoClient = new MongoClient("mongodb://localhost:27017/");
     protected readonly MongoClient MongoClient = new MongoClient("mongodb://157.90.1.251:3512/");
 
     [SetUp]
